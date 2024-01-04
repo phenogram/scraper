b70643dad243666e7cf9f409a8ba466b4bc5ffc0
Nikolai Shangin <shanginn@gmail.com>
2024-01-04 14:16:58 +0700

    add MessageOrigin implementation

---
+++ src/Common/AbstractClassResolvers/MessageOrigin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TgScraper\Common\AbstractClassResolvers;
+
+class MessageOrigin implements AbstractClassResolverInterface
+{
+    public static function getBody(array $params): string
+    {
+        return <<<'BODY'
+            return match ($data['type']) {
+                'user' => $this->denormalizeMessageOriginUser($data),
+                'hidden_user' => $this->denormalizeMessageOriginHiddenUser($data),
+                'chat' => $this->denormalizeMessageOriginChat($data),
+                'channel' => $this->denormalizeMessageOriginChannel($data),
+                default => throw new \InvalidArgumentException(sprintf('Invalid type "%s" given. Supported types are: "user", "hidden_user", "chat", "channel"', $data['type'])),
+            };
+            BODY;
+    }
+}
